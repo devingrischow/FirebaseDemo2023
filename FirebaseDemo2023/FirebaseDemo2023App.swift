9b9ba56b8cf80115f2b11e41268451e6d8de78c7
devingrischow <devingrischow@outlook.com>
2023-10-26 17:23:52 -0500

    Initial Commit

---
+++ FirebaseDemo2023/FirebaseDemo2023App.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseDemo2023App.swift
+//  FirebaseDemo2023
+//
+//  Created by Devin Grischow on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseDemo2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
